58d72827719d808f297380570477e2794a64cf2d
Stanciu Alexandru <alexandru_stanciu20@yahoo.com>
2024-01-26 18:39:00 +0200

    Verificare numar in numere extrase - fara a folosi operatorul in

--- player/number.py
+++ player/number.py
@@ -6,3 +6,12 @@ print(numere)
 numar_ales = int(input("Introduceti un numar intre 1 si 49:\n"))
 print("Ai ghicit!") if numar_ales in numere else print("Nu ai ghicit!")
 
+ai_ghicit = []
+for nr in numere:
+    if nr == numar_ales:
+        ai_ghicit.append(nr)
+
+if len(ai_ghicit) == 0:
+    print("Nu ai ghicit!")
+else:
+    print("Ai ghicit!")
