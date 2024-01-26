0297a1708d36b12561758d851aa4a623e7245f52
Stanciu Alexandru <alexandru_stanciu20@yahoo.com>
2024-01-26 19:52:28 +0200

    Returneaza numere comune la 6 numere extrase

--- player/number.py
+++ player/number.py
@@ -3,13 +3,28 @@ import random
 numere = [random.randint(1,49) for i in range(6)]
 print(numere)
 
-numar_ales = int(input("Introduceti un numar intre 1 si 49:\n"))
+numere_alese = [int(input("Introduceti un numar intre 1 si 49:\n")) for i in range(6)]
 
-for nr in numere:
-    if numar_ales == nr:
-        print("Ai ghicit!")
-        break
-else:
-    print("Nu ai ghicit!")
+nr_ghicite = [nr for nr in numere_alese if nr in numere]
+print(nr_ghicite)
 
-print(any([numar_ales == nr for nr in numere]))
+categorii = {
+    6 : "Categoria 1",
+    5 : "Categoria 2",
+    4 : "Categoria 3",
+    3 : "Categoria 4"
+}
+
+print(categorii[len(nr_ghicite)])
+
+
+
+
+# for nr in numere:
+#     if numar_ales == nr:
+#         print("Ai ghicit!")
+#         break
+# else:
+#     print("Nu ai ghicit!")
+
+# print(any([numar_ales == nr for nr in numere]))
