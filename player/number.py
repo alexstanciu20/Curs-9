5b17ed6cc5e42350cce9df1160e2bab25a7e6f08
Stanciu Alexandru <alexandru_stanciu20@yahoo.com>
2024-01-26 19:56:34 +0200

    Refactorizare a listelor folosind list comprehension

--- player/number.py
+++ player/number.py
@@ -1,11 +1,10 @@
 import random
 
 numere = [random.randint(1,49) for i in range(6)]
-print(numere)
-
 numere_alese = [int(input("Introduceti un numar intre 1 si 49:\n")) for i in range(6)]
-
 nr_ghicite = [nr for nr in numere_alese if nr in numere]
+
+print(numere)
 print(nr_ghicite)
 
 categorii = {
@@ -16,15 +15,3 @@ categorii = {
 }
 
 print(categorii[len(nr_ghicite)])
-
-
-
-
-# for nr in numere:
-#     if numar_ales == nr:
-#         print("Ai ghicit!")
-#         break
-# else:
-#     print("Nu ai ghicit!")
-
-# print(any([numar_ales == nr for nr in numere]))
