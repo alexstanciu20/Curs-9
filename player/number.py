fbdd5c026deccc9f036a289521fd638eeab8e03e
Stanciu Alexandru <alexandru_stanciu20@yahoo.com>
2024-01-26 18:24:15 +0200

    Verifica daca un numar sa afla in lista de numere

--- player/number.py
+++ player/number.py
@@ -3,4 +3,6 @@ import random
 numere = [random.randint(1,49) for i in range(6)]
 print(numere)
 
+numar_ales = int(input("Introduceti un numar intre 1 si 49:\n"))
+print("Ai ghicit!") if numar_ales in numere else print("Nu ai ghicit!")
 
