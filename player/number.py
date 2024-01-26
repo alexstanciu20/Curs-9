7def77cdc0948acb54c3f5d593c4a26080a92bc8
Stanciu Alexandru <alexandru_stanciu20@yahoo.com>
2024-01-26 20:07:24 +0200

    Salvarea unei categorii

--- player/number.py
+++ player/number.py
@@ -1,17 +1,18 @@
 import random
 
 numere = [random.randint(1,49) for i in range(6)]
+print(numere)
 numere_alese = [int(input("Introduceti un numar intre 1 si 49:\n")) for i in range(6)]
 nr_ghicite = [nr for nr in numere_alese if nr in numere]
 
-print(numere)
 print(nr_ghicite)
 
 categorii = {
-    6 : "Categoria 1",
-    5 : "Categoria 2",
-    4 : "Categoria 3",
-    3 : "Categoria 4"
+    6 : 1,
+    5 : 2,
+    4 : 3,
+    3 : 4
 }
 
-print(categorii[len(nr_ghicite)])
+print("Categoria: ", categorii[len(nr_ghicite)] if len(nr_ghicite) in categorii else 0)
+print("Categoria: ", categorii.get(len(nr_ghicite), 0))
